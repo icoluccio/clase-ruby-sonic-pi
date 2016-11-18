5142c4de8b6276b6caff3e53b5e37a4387ff8c3f
Ignacio Coluccio <icoluccio@users.noreply.github.com>
2016-11-18 05:35:29 +0200

    OOP

--- sonicpitest.rb
+++ sonicpitest.rb
@@ -1,123 +1,76 @@
-time = 0.18
+time = 0.22
+
+kick = Percusion.new(:kick, "C:/Users/Pedro Soto/Desktop/kick.wav", [3, 0, 0, 0], true)
+##| kick = Percusion.new(:kick, "C:/Users/Pedro Soto/Desktop/kick.wav", [3, 0, 0, 0, 0, 0, 2, 0, 0, 3, 0, 3, 0, 0, 0, 2], true)
+hihat = Percusion.new(:hihat, "C:/Users/Pedro Soto/Desktop/hat.wav", [0.5, 0.6, 0.8, 0.8], true)
+snare = Percusion.new(:snare, "C:/Users/Pedro Soto/Desktop/snare.wav", [0, 0, 0, 0, 1, 0 , 0 ,0, 0, 0, 0, 0, 1, 0 , 0 ,0], true)
+percusiones = [kick, hihat, snare]
+bajo = Bajo.new([nil, 53, 53, 53] * 4 + [nil, 55, 55, 55] * 4 + [nil, 50, 50, 50] * 8 +
+                [nil, 53, 53, 53] * 4 + [nil, 55, 55, 55] * 4 + [nil, 50, 50, 50] * 8 +
+                [nil, 48, 48, 48] * 4 + [nil, 47, 47, 47] * 4 + [nil, 57, 57, 57] * 4 +  [nil, 50, 50, 50] * 4, :dpulse)
+##| bajo = Bajo.new([48,60]* 8 + [55,67] * 8 + [57,69] * 8 + [50,62] * 8, :dpulse)
+##| lead = Lead.new([nil,76,nil,69,nil,88, 86,88], :dtri)
+
+lead = Lead.new([nil,67,69,64,nil,nil, nil,nil] * 4 +
+                [nil,67,69,76,nil,nil, nil,nil,
+                 nil,67,69,74,nil,nil, nil,nil] * 2, :dtri)
+
+acordes = Acordes.new([69,72,81,84], :fm)
+
+
+lead2 =  Lead.new([67, 64, 60, 69,
+                   64, 72, 76, 69,
+                   60, 69, 64, 60,
+                   60, nil, nil, nil], :chiplead)
+
+amen = Amen.new(2)
+
+
+
+Volumen.cambiarVolumen(kick, 0)
+Volumen.cambiarVolumen(hihat, 0)
+Volumen.cambiarVolumen(snare, 0)
+Volumen.cambiarVolumen(bajo, 1)
+Volumen.cambiarVolumen(lead, 1)
+Volumen.cambiarVolumen(lead2, 0)
+Volumen.cambiarVolumen(acordes, 0)
+Volumen.cambiarVolumen(amen, 0)
+
 
 live_loop :sync do
-  cues = [[:complete, :quarter_tick, :half_tick, :tick],
-          [:quarter_tick],
-          [:half_tick, :quarter_tick],
-          [:quarter_tick],
-          [:half_tick, :quarter_tick],
-          [:quarter_tick],
-          [:quarter_tick, :half_tick, :tick],
-          [:quarter_tick],
-          [:half_tick, :quarter_tick],
-          [:quarter_tick],
-          [:half_tick, :quarter_tick],
-          [:quarter_tick]].ring.tick
-  cues.map { |c| cue c }
-  sleep time
+  Sync.new(self).sync
 end
 
 sync :complete
 
-class Percusion
-  attr_accessor :sonido, :amps, :nombre, :prendido
-  def initialize(nombre, sonido, amps, prendido = true)
-    self.nombre = nombre
-    self.sonido = sonido
-    self.amps = amps
-    self.prendido = prendido
-  end
-  
-  def tiempo
-    :quarter_tick
-  end
-  
-  def proxima_amp
-    return 0 unless prendido
-    amps.ring.tick
-  end
-end
-
-class PercusionAlAzar < Percusion
-  def proxima_amp
-    return 0 unless prendido
-    rand(10)
-  end
-end
-
-
-kick = Percusion.new(:kick, :drum_bass_hard, [4, 0, 0, 0], true)
-# kick = Percusion.new(:kick, :drum_bass_hard, [3, 0, 0, 0, 0, 0, 2, 0, 0, 3, 0, 3, 0, 0, 0, 2], true)
-hihat = Percusion.new(:hihat, :drum_cymbal_closed, [0.5, 0.6, 0.8, 0.8], false)
-snare = Percusion.new(:snare, :drum_snare_hard, [0, 0, 0, 0, 1, 0 , 0 ,0, 0, 0, 0, 0, 1, 0 , 0 ,0], false)
-# percusiones = [kick, hihat, snare]
-percusiones = [kick, hihat, snare]
-
-live_loop :bass do
-  with_fx :distortion, mix:0.5, distort:0.9 do
-    sync :quarter_tick
-    use_synth :dsaw
-    melody =
-      [nil, 53, 53, 53] * 4 + [nil, 55, 55, 55] * 4 + [nil, 50, 50, 50] * 8 +
-      [nil, 53, 53, 53] * 4 + [nil, 55, 55, 55] * 4 + [nil, 50, 50, 50] * 8 +
-      [nil, 48, 48, 48] * 4 + [nil, 47, 47, 47] * 4 + [nil, 57, 57, 57] * 4 +  [nil, 50, 50, 50] * 4
-    note = melody.ring.tick
-    
-    ##| play [note - 12, note -24], release: 0.6, release: 0.2 if note
-  end
-end
-
 percusiones.each do |percusion|
   live_loop (percusion.nombre) do
-    sync percusion.tiempo
-    sample percusion.sonido, amp: percusion.proxima_amp
+    sync :quarter_tick
+    percusion.tocar(self)
   end
+end
+
+live_loop :bass do
+  sync :quarter_tick
+  bajo.tocar(self)
 end
 
 live_loop :lead do
-  use_synth :dtri
-  with_fx :echo, mix:0.3 do
-    sync :quarter_tick
-    melody = [nil,67,69,64,nil,nil, nil,nil] * 4 +
-      [nil,67,69,76,nil,nil, nil,nil,
-       nil,67,69,74,nil,nil, nil,nil] * 2
-    note = melody.ring.tick
-    ##| play [note, note+12, note-12], release: 0.2, attack: 0.02, sustain: 0.1, amp: 1.5
-  end
+  sync :quarter_tick
+  lead.tocar(self)
+end
+
+live_loop :chords do
+  sync :quarter_tick
+  acordes.tocar(self)
+end
+
+live_loop :lead3 do
+  sync :quarter_tick
+  lead2.tocar(self)
 end
 
 live_loop :amen do
-  sync :quarter_tick
-  
-  with_fx :distortion do
-    note_amount = rand(1.5).round + 1
-    notes = []
-    note_amount.times do
-      notes.push([0,5,11,12, 12].sample)
-    end
-    notes.uniq.map { |note|
-      sample :loop_amen, onset: note
-      sleep time/notes.size
-    }
-  end
+  amen.tocar(self)
 end
 
-##| live_loop :chords do
-##|   use_synth :fm
-##|   with_fx :echo, mix:0.5 do
-##|     with_fx :reverb, mix:0.3 do
-##|       sync :quarter_tick
-##|       note = [69,72,81, 84].sample
-##|       play [note, note+7, note+12], release: 0.4, amp: 1 if one_in(2)
-##|     end
-##|   end
-##| end
-
-live_loop :lead2 do
-  sync :quarter_tick
-  use_synth :chiplead
-  note = [100, 96, 103, 100, 96, 105, 100, 96, 112, 100, 96, 105].ring.tick
-  with_fx :ixi_techno, mix: 0.8 do
-    ##| play [note - 24], amp: 2
-  end
-end
---
+++ sonicpitestclasses.rb
@@ -0,0 +1,147 @@
+
+
+class Sync
+  attr_accessor :song
+  def initialize(song)
+    self.song = song
+  end
+  
+  def sync
+    time = 0.2
+    cues = [[:quarter_tick, :half_tick, :tick],
+            [:quarter_tick],
+            [:half_tick, :quarter_tick],
+            [:quarter_tick],
+            [:half_tick, :quarter_tick],
+            [:quarter_tick],
+            [:quarter_tick, :half_tick, :tick],
+            [:quarter_tick],
+            [:half_tick, :quarter_tick],
+            [:quarter_tick],
+            [:half_tick, :quarter_tick],
+            [:quarter_tick, :complete]].ring.tick
+    cues.map { |c| song.cue c }
+    song.sleep time
+  end
+end
+
+class Volumen
+  @@volumenes = {}
+  def self.cambiarVolumen(algo, volumen)
+    @@volumenes[algo] = volumen
+  end
+  
+  def self.obtenerVolumen(algo)
+    return @@volumenes[algo] if !@@volumenes[algo].nil?
+    1
+  end
+end
+
+class Percusion
+  attr_accessor :sonido, :amps, :nombre, :prendido
+  def initialize(nombre, sonido, amps, prendido = true)
+    self.nombre = nombre
+    self.sonido = sonido
+    self.amps = amps
+    self.prendido = prendido
+  end
+  
+  def tocar(cancion)
+    cancion.sample sonido, amp: proxima_amp
+  end
+  
+  def tiempo
+    :quarter_tick
+  end
+  
+  def proxima_amp
+    return 0 unless prendido
+    amps.ring.tick * Volumen.obtenerVolumen(self)
+  end
+end
+
+class PercusionAlAzar < Percusion
+  def proxima_amp
+    return 0 unless prendido
+    rand(10)
+  end
+end
+
+
+class Instrumento
+  attr_accessor :melodia, :sonido
+  def initialize(melodia, sonido)
+    self.melodia = melodia
+    self.sonido = sonido
+  end
+  
+  def proxima_nota
+    melodia.ring.tick
+  end
+  
+  def tocar(cancion)
+    cancion.use_synth sonido
+    hacer_sonido(cancion)
+  end
+  
+  def obtener_volumen
+    Volumen.obtenerVolumen(self)
+  end
+  
+end
+
+
+class Bajo < Instrumento
+  def hacer_sonido(cancion)
+    cancion.with_fx :distortion, mix:0.5, distort:0.9 do
+      note = proxima_nota
+      cancion.play [note - 12, note -24, note], release: 0.6, release: 0.2, amp: obtener_volumen if note
+    end
+  end
+end
+
+class Lead < Instrumento
+  def hacer_sonido(cancion)
+    cancion.with_fx :echo, mix:0.3 do
+      note = proxima_nota
+      cancion.play [note, note+12, note-12], release: 0.2, attack: 0.02, sustain: 0.1, amp: obtener_volumen
+    end
+  end
+end
+
+class Acordes < Instrumento
+  def hacer_sonido(cancion)
+    cancion.with_fx :echo, mix:0.5 do
+      cancion.with_fx :reverb, mix:0.3 do
+        note = proxima_nota
+        cancion.play [note, note+7, note+12], release: 0.4, amp: obtener_volumen if cancion.one_in(2)
+      end
+    end
+    
+    def proxima_nota
+      melodia.sample
+    end
+  end
+end
+
+
+class Amen < Instrumento
+  attr_accessor :velocidad
+  def initialize(velocidad = 2)
+    self.velocidad = velocidad
+  end
+  def tocar(cancion)
+    cancion.sync :quarter_tick
+    cancion.with_fx :distortion do
+      note_amount = rand(1.5).round + velocidad
+      notes = []
+      note_amount.times do
+        notes.push([0,5,11,12, 12].sample)
+      end
+      notes.uniq.map { |note|
+        cancion.sample :loop_amen, onset: note, amp: obtener_volumen
+        cancion.sleep 0.2/notes.size
+      }
+    end
+  end
+end
